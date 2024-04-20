0e1d4c39f15b0a53ed9b280ab514f735a9da63de
aliciamb86 <alicia.mb86@gmail.com>
2024-04-20 20:03:37 +0200

    20/04/2024 20:03

--- Interstellar Travel Customer Satisfaction Analysis/app/main.py
+++ Interstellar Travel Customer Satisfaction Analysis/app/main.py
@@ -3,19 +3,33 @@ from PIL import Image
 import pandas as pd
 import streamlit.components.v1 as c
 
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import streamlit.components.v1 as c
 
 st.set_page_config(page_title="Interstellar Travel Customer Satisfaction Analysis",
                    page_icon="img/nave_2.ico")
 
-seleccion = st.sidebar.selectbox("Selecciona menu", ["Conoce nuestros destinos", "Diseña tu viaje"])
+seleccion = st.sidebar.selectbox("Select menu", ["Home", "Explore our destinations", "Design your trip"])
 
 if seleccion == "Home":
-    st.title("Viajes interestelares")
-
-    # with st.expander("¿Qué es esta aplicación?"):
-    #     st.write("Es una primera aproximación para solucionar la búsqueda de cargadores eléctricos para facilitar la transición a otras fuentes de energía")
-
+    st.title("Interstellar Travel")
+ 
     img = Image.open("img/viajes-interestelares.jpg")
     st.image(img)
-# elif seleccion == "EDA":
-#     st.write("Aquí irá el EDA")
+
+elif seleccion == "Design your trip":
+    st.write("Design your trip to get the best experience")
+    img = Image.open("img/odisea-interestelar-800x445.jpg")
+    st.image(img)
+
+    age = st.number_input("Enter your age", min_value=0, max_value=150, value=0)
+
+    gender = st.selectbox("Enter your gender", ["Male", "Female"])
+
+    occupation = st.selectbox("Enter your occupation", ["Scientist", "Businessperson", "Explorer", "Tourist", "Colonist", "Other"])
+
+    purpose_of_travel = st.selectbox("Enter your purpose of travel", ["Tourism", "Research", "Business", "Colonization", "Other"])
+
+    
